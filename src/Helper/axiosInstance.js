63aabe2cd95d1a571897c824a77a9ede27690eff
aswin-codes <codicon.co@gmail.com>
2024-04-15 08:33:01 +0530

    axios instance

--- src/Helper/axiosInstance.js
+++ src/Helper/axiosInstance.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = "http://localhost:3000/api/";
+const BASE_URL = "https://bisinenode.vercel.app/api/";
 
 const axiosInstance = axios.create();
 
--- src/axios.js
+++ src/axios.js
@@ -1,5 +1,5 @@
 import axios from 'axios'
-const baseURL = 'http://127.0.0.1:3000/api/'
+const baseURL = 'https://bisinenode.vercel.app/api/'
 
 const axiosInstance = axios.create({
     baseURL: baseURL,
