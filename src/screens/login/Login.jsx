da265f2a08de022196454075bccdca30b5443fd2
aswin-codes <codicon.co@gmail.com>
2024-04-26 14:48:33 +0530

    feat : test username and password input

--- src/screens/login/Login.jsx
+++ src/screens/login/Login.jsx
@@ -1,11 +1,52 @@
-import {useEffect} from 'react'
+import {useEffect, useState} from 'react'
 import Vector from './vector.png'
+import axios from 'axios'
 //import { gapi } from 'gapi-script'
 import LoginButton from './LoginButton'
+import { Button, Input } from '@nextui-org/react'
+import { useNavigate } from 'react-router-dom'
 
 const clientId = "774961232823-b6fmrl6p9tcbhgk19fuv7a6ftbbegcm6.apps.googleusercontent.com"
 const LoginSide = () => {
-  
+  const [username, setUsername] = useState("")
+  const [password, setPassword] = useState("")
+  const [isLoading, setIsLoading] = useState(false)
+  const navigate = useNavigate()
+
+  const handleLogin = () => {
+    const reqBody = {username,password}
+    setIsLoading(true);
+    axios
+      .post(`https://bisinenode.vercel.app/api/user/test`, reqBody)
+      .then((res) => {
+        setIsLoading(false);
+        if (res.status == 200) 
+        {
+          localStorage.setItem("profileUrl","https://lh3.googleusercontent.com/a/ACg8ocIgBcCyKE3AVVCFsxL6b3vJlmB-iQBB11VEx_x4G9gATgAeZw=s96-c")
+          console.log(res.data)
+          const { user, access_token,shop } = res.data;
+          // You can handle the response accordingly, such as storing user details or token in local storage
+          localStorage.setItem("user", JSON.stringify(user));
+          localStorage.setItem("shop",JSON.stringify(shop))
+          localStorage.setItem("access_token", access_token);
+          navigate("/");
+        }
+      })
+      .catch((error) => {
+        if (error.response && error.response.status === 404) {
+          // Handle 404 error (user not found)
+          console.log(error.response);
+          localStorage.setItem("shop",JSON.stringify({shopId:null}))
+          navigate("/user/register"); // Navigate to signup page or another page
+        } else {
+          // Handle other errors
+          console.error("Error:", error);
+          // Handle error response from backend
+        }
+        // Handle error response from backend
+      });
+    
+  }
   
   //Old version
   // useEffect(()=>{
@@ -23,6 +64,11 @@ const LoginSide = () => {
         <h1 className='text-3xl font-semibold text-black'>Get Started</h1>
         <div>
             <img src={Vector} className='object-contain h-64 lg:h-96'/>
+        </div>
+        <div className='flex flex-col gap-3 w-72 mb-5'>
+          <Input value={username} onValueChange={(e) => setUsername(e)} label="username" color='primary' />
+          <Input value={password} onValueChange={(e) => setPassword(e)} label="password" color='primary' type='password' />
+          <Button onClick={handleLogin} color='primary' isLoading={isLoading} className='font-semibold'>Login</Button>
         </div>
         <LoginButton/>
     </div>
